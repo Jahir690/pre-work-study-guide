e728ddfe06c87bef042bdf4b2515a16ce686968c
Jahir690 <jahirm690@outlook.com>
2023-05-04 22:54:49 -0500

    forgot to save

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
-
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -23,9 +22,6 @@ function selectTopics() {
 }
 
 console.log('Here are the topics we learned through Prework:');
-
 listTopics()
-
 console.log('Which topic should we study first?');
-
 selectTopics()
